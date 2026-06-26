e573e23807f395adb28b2fbfe08f50d01e65ae4f
Stanisław Borodziuk <borodziukstanislaw@gmail.com>
2026-06-27 01:38:46 +0200

    feat: embed client-side PDF viewer

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,19 +4,12 @@ import sitemap from '@astrojs/sitemap'
 import alpinejs from '@astrojs/alpinejs'
 import { defineConfig } from 'astro/config'
 
-
 // https://astro.build/config
 export default defineConfig({
     site: 'https://meet.jbwk.pl',
     redirects: {
-        '/meet-2025-regulamin': '/JBWK Meet Warszawa 2025 Regulamin.pdf',
-        '/meet-2025-zgoda-rodzica':
-            '/JBWK Meet Warszawa 2025 Zgoda Rodzica.pdf',
         '/meet-2025-rejestracja':
             'https://docs.google.com/forms/d/e/1FAIpQLSd2968UPuHfbYiEdGpKMndIxNjF5kFr_E8OLsrs5ZBR6KYOuQ/viewform',
-        '/meet-2026-regulamin': '/JBWK Meet Warszawa 2026 Regulamin.pdf',
-        '/meet-2026-zgoda-rodzica':
-            '/JBWK Meet Warszawa 2026 Zgoda Rodzica.pdf',
         '/meet-2026-rejestracja': 'https://forms.gle/bfRhUFYgonxFb74Z6',
     },
 
